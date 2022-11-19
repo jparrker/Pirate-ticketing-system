76092214b7c6a1774a5cadd036230643a2fca6ed
jparrker <102701917+jparrker@users.noreply.github.com>
2022-11-19 17:29:42 -0500

    finished form and added controller for add new ticket and delete ticket

--- routes/edit.js
+++ routes/edit.js
@@ -4,8 +4,7 @@ const express = require('express')
 const router = express.Router()
 const editController = require('../controllers/edit')
 
-router.get('/:id', editController.getEdit)
-router.get('/remove/:id', editController.deleteTask)
+router.get('/remove/:id', editController.deleteTicket)
 router.post('/:id', editController.updateTask)
 
 module.exports = router
